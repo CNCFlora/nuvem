a4f10b2f7882577b5e1b635d32c54e5d207c17a8
Diogo Souza <diogo@diogok.net>
2014-09-26 07:50:22 -0300

    update run script

--- run.sh
+++ run.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-export HOST=$(hostname -I | awk '{ print $1 }')
-export ETCD=http://$HOST/etcd
+[[ ! $HOST ]] && HOST=$(hostname -I | awk '{ print $1 }')
+[[ ! $ETCD ]] && ETCD=http://$HOST/etcd
 export ROOT_APP=entrypoint
+
+export HOST
+export ETCD
 
 fige -c 'docker2etcd -u' -t etcd
 sleep 2
 fige -c 'docker2etcd -u' -t logstash
 sleep 5
+fige -c 'docker2etcd -u' -t entrypoint
+sleep 5
+fige -c 'docker2etcd -u' -t proxy
+sleep 5
 fige -c 'docker2etcd -u' -t datahub
 sleep 5
 
 for n in $(cat fig.yml | grep name  | awk '{ print $2 }'  ); do
-    if [ "$n" != "logstash" ] && [ "$n" != "datahub" ] && [ "$n" != "etcd" ] ; then
-        echo "run $n"
+    if [ "$n" != "logstash" ] && [ "$n" != "datahub" ] && [ "$n" != "etcd" ] && [ "$n" != "entrypoint" ] && [ "$n" != "proxy" ] ; then
+        fige -c 'docker2etcd -u' -t $n
     fi
 done
 
