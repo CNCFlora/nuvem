78f54ac16fb737567b8facd7ef2ffc4b55753aa1
Diogo Souza <diogo@diogok.net>
2015-05-26 12:02:53 -0300

    update scripts to run

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(cat fig.yml | grep image  | awk '{ print $2 }' | sort | uniq ); do
+for i in $(cat docker-compose.yml | grep image  | awk '{ print $2 }' | sort | uniq ); do
     docker pull $i
 done
 
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for n in $(cat fig.yml | grep name | awk '{ print $2 }' ); do
+for n in $(cat docker-compose.yml | grep name | awk '{ print $2 }' ); do
   docker stop $n
 done
 
