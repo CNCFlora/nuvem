7af12c8ca91aedf4fdddb2665a8d85002f2f37ad
Diogo Souza <diogo@diogok.net>
2015-06-16 15:51:27 -0300

    no more need for this config

--- cfg.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-curl -X PUT http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/url -d 'https://hooks.slack.com/services/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'
-curl -X PUT http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel -d '#cncflora-monitor'
-curl -X PUT http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/username -d 'diogok'
-curl -X PUT http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/icon-emoji -d ':chart_with_downwards_trend:'
-curl -X PUT http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled -d 'true'
