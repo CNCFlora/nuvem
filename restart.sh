ef28fc181426f655a3b14a7e6c44e043c3d89c1d
Diogo Silva <diogo@diogok.net>
2016-06-27 21:23:32 -0300

    using docker-compose pull instead of docker pull

--- restart.sh
+++ restart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker pull cncflora/$1
-
-docker-compose stop $1 && docker-compose rm $1 && docker-compose up -d $1
+docker-compose pull $1 && docker-compose stop $1 && docker-compose rm $1 && docker-compose up -d $1
 
